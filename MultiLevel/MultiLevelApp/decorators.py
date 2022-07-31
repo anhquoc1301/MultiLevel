f5e785c47fc4e910936a9fbf1c5b8cb0324ce5e7
anhquoc1301 <anhquoc1301@gmail.com>
2022-08-01 06:34:32 +0700

    first commit

---
+++ MultiLevel/MultiLevelApp/decorators.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+def admin_only(view_func):
+    def function(request, *args, **kwargs):
+        user=request.user.username
+        if user=='admin':
+            return view_func(request, *args, **kwargs)
+        else:
+            return HttpResponse("Chức năng này chỉ dành cho Admin!")
+    return function
